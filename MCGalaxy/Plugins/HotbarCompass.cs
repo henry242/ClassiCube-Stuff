9d7b20413e39f32e29c95fc99fc70ec364a9af5c
henry242 <76452231+henry242@users.noreply.github.com>
2021-05-08 20:48:25 -0500

    Add Readme and +compass
    +compass is for if you want the compass there or not

--- MCGalaxy/Plugins/HotbarCompass.cs
+++ MCGalaxy/Plugins/HotbarCompass.cs
@@ -1,3 +1,6 @@
+//INSTALL INSTRUCTIONS
+//Make level blocks 100-107 going clockwise from bottom. For instance: bottom, bottom-left, left, etc.
+//Use +compass in the motd to use this.
 using System;
 using System.Threading;
 
@@ -23,37 +26,39 @@ namespace MCGalaxy {
         void CheckDirection(SchedulerTask tak) {
             Player[] players = PlayerInfo.Online.Items;
             foreach (Player p in players) {
-                if (Orientation.PackedToDegrees(p.Rot.RotY) >= 339 && Orientation.PackedToDegrees(p.Rot.RotY) < 361 || Orientation.PackedToDegrees(p.Rot.RotY) >= 0 && Orientation.PackedToDegrees(p.Rot.RotY) < 33) {
-                    p.Send(Packet.SetHotbar(100, 8, true));
-                }
-                
-                if (Orientation.PackedToDegrees(p.Rot.RotY) >= 33 && Orientation.PackedToDegrees(p.Rot.RotY) < 68) {
-                    p.Send(Packet.SetHotbar(101, 8, true));
-                }
-                
-                if (Orientation.PackedToDegrees(p.Rot.RotY) >= 68 && Orientation.PackedToDegrees(p.Rot.RotY) < 113) {
-                    p.Send(Packet.SetHotbar(102, 8, true));
-                }
-                
-                if (Orientation.PackedToDegrees(p.Rot.RotY) >= 113 && Orientation.PackedToDegrees(p.Rot.RotY) < 158) {
-                    p.Send(Packet.SetHotbar(103, 8, true));
-                }
-                
-                if (Orientation.PackedToDegrees(p.Rot.RotY) >= 153 && Orientation.PackedToDegrees(p.Rot.RotY) < 203) {
-                    p.Send(Packet.SetHotbar(104 , 8, true));
-                }
-                
-                if (Orientation.PackedToDegrees(p.Rot.RotY) >= 203 && Orientation.PackedToDegrees(p.Rot.RotY) < 258) {
-                    p.Send(Packet.SetHotbar(105, 8, true));
-                }
-                
-                if (Orientation.PackedToDegrees(p.Rot.RotY) >= 258 && Orientation.PackedToDegrees(p.Rot.RotY) < 293) {
-                    p.Send(Packet.SetHotbar(106, 8, true));
-                }
-                
-                if (Orientation.PackedToDegrees(p.Rot.RotY) >= 293 && Orientation.PackedToDegrees(p.Rot.RotY) < 339) {
-                    p.Send(Packet.SetHotbar(107, 8, true));
-                }
+	        if (p.level.Config.MOTD.ToLower().Contains("+compass")) {
+			if (Orientation.PackedToDegrees(p.Rot.RotY) >= 339 && Orientation.PackedToDegrees(p.Rot.RotY) < 361 || Orientation.PackedToDegrees(p.Rot.RotY) >= 0 && Orientation.PackedToDegrees(p.Rot.RotY) < 33) {
+			    p.Send(Packet.SetHotbar(100, 8, true));
+			}
+
+			if (Orientation.PackedToDegrees(p.Rot.RotY) >= 33 && Orientation.PackedToDegrees(p.Rot.RotY) < 68) {
+			    p.Send(Packet.SetHotbar(101, 8, true));
+			}
+
+			if (Orientation.PackedToDegrees(p.Rot.RotY) >= 68 && Orientation.PackedToDegrees(p.Rot.RotY) < 113) {
+			    p.Send(Packet.SetHotbar(102, 8, true));
+			}
+
+			if (Orientation.PackedToDegrees(p.Rot.RotY) >= 113 && Orientation.PackedToDegrees(p.Rot.RotY) < 158) {
+			    p.Send(Packet.SetHotbar(103, 8, true));
+			}
+
+			if (Orientation.PackedToDegrees(p.Rot.RotY) >= 153 && Orientation.PackedToDegrees(p.Rot.RotY) < 203) {
+			    p.Send(Packet.SetHotbar(104 , 8, true));
+			}
+
+			if (Orientation.PackedToDegrees(p.Rot.RotY) >= 203 && Orientation.PackedToDegrees(p.Rot.RotY) < 258) {
+			    p.Send(Packet.SetHotbar(105, 8, true));
+			}
+
+			if (Orientation.PackedToDegrees(p.Rot.RotY) >= 258 && Orientation.PackedToDegrees(p.Rot.RotY) < 293) {
+			    p.Send(Packet.SetHotbar(106, 8, true));
+			}
+
+			if (Orientation.PackedToDegrees(p.Rot.RotY) >= 293 && Orientation.PackedToDegrees(p.Rot.RotY) < 339) {
+			    p.Send(Packet.SetHotbar(107, 8, true));
+			}
+		}
             }
         }
 
